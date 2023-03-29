e861cd7e4d3875594db887a61e72af712d3ba835
Petros Koutsolampros <commits@pklampros.io>
2023-03-29 01:49:41 +0100

    Add mailparse and php-mime-mail-parser

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -7,10 +7,19 @@ namespace OCA\Athenaeum\AppInfo;
 
 use OCP\AppFramework\App;
 
+include_once __DIR__ . '/../../vendor/autoload.php';
+
 class Application extends App {
 	public const APP_ID = 'athenaeum';
 
 	public function __construct() {
 		parent::__construct(self::APP_ID);
 	}
+
+    public function register(IRegistrationContext $context): void {
+        $context->registerEventListener(
+            BeforeUserDeletedEvent::class,
+            UserDeletedListener::class
+        );
+    }
 }
