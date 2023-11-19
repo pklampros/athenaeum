23c7a0bf47c3c6cee109f04cd7f56f4de04f56dc
Petros Koutsolampros <commits@orange-vertex.net>
2023-11-19 19:46:11 +0200

    Separate building the container from running it

--- container/makeAppContainer.sh
+++ container/makeAppContainer.sh
@@ -3,25 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-# install container
-podman build -t athenaeum-builder \
---build-arg PHP_EXTRA_CONFIGURE_ARGS="--enable-mailparse" \
--<< EOF
-FROM php:8.2-cli
-RUN apt-get update && apt-get install -y npm
-RUN pecl install mailparse && docker-php-ext-enable mailparse
-ENV COMPOSER_ALLOW_SUPERUSER=1
-#ENV build_tools_directory="/usr/bin"
-RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
-
-#RUN curl -sS https://getcomposer.org/installer | php -d memory_limit=4096M && \
-#    mv composer.phar /usr/local/bin && \\
-#    php -d memory_limit=4096M /usr/local/bin/composer.phar install --prefer-dist
-
-#CMD mv /usr/local/bin/composer.phar /src/build/ && /src/makeApp.sh n
-CMD /src/makeApp.sh n
-EOF
+#podman start athenaeum-builder:latest
 
 podman run \
     --volume=../:/src/ \
