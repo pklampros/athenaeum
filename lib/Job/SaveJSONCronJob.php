0cd6f3a8f28be31f832f4ed4c52f42af63636b6d
Petros Koutsolampros <commits@orange-vertex.net>
2024-10-21 03:09:35 +0300

    Add missing headers

--- lib/Job/SaveJSONCronJob.php
+++ lib/Job/SaveJSONCronJob.php
@@ -1,4 +1,9 @@
 <?php
+
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Petros Koutsolampros <commits@pklampros.io>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 namespace OCA\Athenaeum\Job;
 
 use OCA\Athenaeum\Service\ItemService;
